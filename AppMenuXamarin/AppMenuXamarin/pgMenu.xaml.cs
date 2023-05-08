1de2106530d15b7c9020f5663b03f498918db4f0
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2023-05-08 10:45:56 -0300

    AppMenuXamarin
    Side Menu Xamarin.Forms

---
+++ AppMenuXamarin/AppMenuXamarin/pgMenu.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMenuXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pgMenu : Shell
+    {
+        public pgMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
